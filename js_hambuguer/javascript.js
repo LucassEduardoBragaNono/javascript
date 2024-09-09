8e76b33618239214e704e3b53061c070066896ca
LucassEduardoBragaNono <lucasnono1823@gmail.com>
2024-09-09 18:44:24 -0300

    menu_hamb

---
+++ js_hambuguer/javascript.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
